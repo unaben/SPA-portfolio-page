6ad583d20709e466e8ad4a4ca3e4b36c8640152f
unaben <unaben@yahoo.com>
2022-04-02 15:04:07 +0100

    update

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-// global.onload = function () {
-//   window.addEventListener("scroll", function (e) {
-//     if (window.pageYOffset > 100) {
-//       document.querySelector("header").classList.add("is-scrolling");
-//     } else {
-//       document.querySelector("header").classList.remove("is-scrolling");
-//     }
-//   });
+global.onload = function () {
+  window.addEventListener("scroll", function (e) {
+    if (window.pageYOffset > 100) {
+      document.querySelector("header").classList.add("is-scrolling");
+    } else {
+      document.querySelector("header").classList.remove("is-scrolling");
+    }
+  });
 
   const menu_btn = document.querySelector(".hamburger");
   const mobile_menu = document.querySelector(".mobile-nav");
@@ -14,4 +14,4 @@
     menu_btn.classList.toggle("is-active");
     mobile_menu.classList.toggle("is-active");
   });
-// };
+};
